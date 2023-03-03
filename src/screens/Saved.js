a2edcac31cb9fe540c5cbb67a49a48118433aaba
joel-chad <joelchadambuka@gmail.com>
2023-03-03 04:48:07 +0200

    push

--- src/screens/Saved.js
+++ src/screens/Saved.js
@@ -1,21 +1,33 @@
-import React from 'react'
-import {Dimensions,ActivityIndicator,View,Text, ScrollView} from 'react-native';
-import Icon from '@expo/vector-icons/Ionicons'
-import s from '../../styles/mainStyle';
-import { useEffect, useState } from 'react/cjs/react.development';
-import apiData from '../../api/getApi';
-import ProductMini from '../components/universal/productmini';
+
+import React, { PureComponent } from 'react';
+import {
+  StatusBar,
+  StyleSheet,
+  SafeAreaView,
+  Text,
+  View,
+  TouchableOpacity,
+  FlatList
+} from 'react-native';
+import { ListItem, Avatar } from 'react-native-elements';
 import CartServices from '../services/CartServices';
+import Icon from 'react-native-vector-icons/Ionicons'
 
-const {width,height}=Dimensions.get('window');
-export default function Saved(props){
-    const [saved,setSaved]=useState([]);
-    const [isLoading,setisLoading]=useState(true);
+class Saved extends PureComponent {
 
-    const getSavedProducts=(url)=>{
+  constructor(props){
+    super(props);
+    this.state = {
+      data: [],
+      bill: ''
+    }
+  }
+     getSavedProducts=()=>{
         CartServices.getCartItems()
             .then(res=>{
-                setSaved(res)
+                // setSaved(res)
+                this.setState({data: res.items})
+                this.setState({bill: res.bill})
                 console.log(res)
             })
             .catch(err=>{
@@ -23,19 +35,47 @@ export default function Saved(props){
             })
     }
 
-    useEffect(()=>{
-        getSavedProducts();
-    },[]);
+    // useEffect(()=>{
+    //     getSavedProducts();
+    // },[]);
+  
+    renderCats = ({item}) => (
+      <View style={{marginTop: 0}}>
+      <ListItem bottomDivider>
+      {/* <Avatar rounded large source={{uri: 'https://cdn-prod.medicalnewstoday.com/content/images/articles/322/322868/golden-retriever-puppy.jpg'}} height={36} width={36} /> */}
+       <ListItem.Content>
+         <ListItem.Title style={{color:'black', fontSize: 18}}>{item.name}</ListItem.Title>
+         <ListItem.Subtitle style={{color: 'black'}}>{`$${item.price}`}</ListItem.Subtitle>
+         <ListItem.Subtitle style={{color: 'black'}}>{`${item.quantity}units`}</ListItem.Subtitle>
+       </ListItem.Content>
+     </ListItem>
+      </View>
+    );
+  
+  
+    componentDidMount(){
+      this.getSavedProducts()
+    }
+  
+    render(){
+      return(
+        <SafeAreaView style={{ flex: 1}}>
+        <FlatList
+          removeClippedSubviews={true}
+          data={this.state.data}
+          renderItem={item => this.renderCats(item)}
+        />
+        <Text style={styles.total}>Total {this.state.bill}</Text>
+      </SafeAreaView>
+        )
+    }
+  }
 
-    return(
-        <ScrollView>
-           <FlatList
-        data={[
-         saved
-        ]}
-        renderItem={({item}) => 
-        <Text style={styles.item}>{item.key}</Text>}
-      />
-        </ScrollView>
-    )
-}
+  const styles = StyleSheet.create({
+    total: {
+        fontSize: 25,
+        // justifyContent: 'flex-end'
+    }
+  })
+  
+  export default Saved;
